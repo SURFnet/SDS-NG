75135e64b52a0803a002edd67957866775559e2d
Okke Harsta <oharsta@zilverline.com>
2026-06-11 12:17:12 +0200

    Upped version

---
+++ playground/src/declarations.d.ts
@@ -0,0 +1,1 @@
+declare module '@surfnet/sds-ng/styles.css'
---
+++ src/styles.ts
@@ -0,0 +1,3 @@
+// Type declaration for the published CSS entry point.
+// Consumers importing '@surfnet/sds-ng/styles.css' will resolve this file.
+export {}
